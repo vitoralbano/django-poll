2cc8d92e5ea56ecaf5d428c6e7af4f227df70853
Vitor Albano <vitoralbano@gmail.com>
2020-01-20 09:08:29 -0300

    add __str__ method to polls models

--- basic_poll/polls/models.py
+++ basic_poll/polls/models.py
@@ -8,6 +8,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
     def __repr__(self):
         return f"{self.__class__.__name__}(question_text='{self.question_text}', pub_date='{self.pub_date}')"
 
@@ -19,6 +22,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
     def __repr__(self):
         return f"{self.__class__.__name__}(question='{self.question}', choice_text='{self.choice_text}')"
