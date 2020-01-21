73e9dd1154fda932e2cd3dd75df1fdc845e3cea6
Vitor Albano <vitoralbano@gmail.com>
2020-01-20 22:08:23 -0300

    add namespace to urls

--- basic_poll/polls/views.py
+++ basic_poll/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
 from .models import Question
@@ -11,11 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExists:
-        raise Http404("Question does not exist")
-
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
