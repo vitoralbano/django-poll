3c5ecd7e3bc030e8e65d07bf8630b2e33fa236d1
Vitor Albano <vitoralbano@gmail.com>
2020-01-20 22:32:08 -0300

    vote in a choice using post

--- basic_poll/polls/views.py
+++ basic_poll/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
@@ -20,4 +21,25 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': "You didn't selected a choice."
+        }
+        return render(request, 'polls/detail.html', context)
+        
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
