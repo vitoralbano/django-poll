c3f234e5d913f11f0ea897b18430323bd36c75ce
Vitor Albano <vitoralbano@gmail.com>
2020-01-22 23:00:52 -0300

    add unit test polls questions

--- basic_poll/polls/tests.py
+++ basic_poll/polls/tests.py
@@ -13,4 +13,5 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
+
         self.assertIs(future_question.was_published_recently(), False)
