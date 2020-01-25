2bf9feaf353c92fe43b04dec05b4fecc27a5dfea
Vitor Albano <vitoralbano@gmail.com>
2020-01-25 10:11:16 -0300

    list only past and recent questions in Question Index View

--- basic_poll/polls/views.py
+++ basic_poll/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 from django.db.models import F
 
@@ -12,7 +13,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
