62644e74d7d3243375213c882ba188d32d11e026
Vitor Albano <vitoralbano@gmail.com>
2020-02-03 09:03:21 -0300

    admin: add search field to Question form

--- basic_poll/polls/admin.py
+++ basic_poll/polls/admin.py
@@ -17,10 +17,9 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
     list_display = ('question_text', 'pub_date', 'was_published_recently')
-
     inlines = [ChoiceInline]
-
-    list_filter = ['pub_date',]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
